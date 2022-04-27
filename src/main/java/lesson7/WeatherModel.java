15888091810355b84e168d4c35d4e82f064ee3f7
Mila <Pod4asova20@gmail.com>
2022-04-27 13:34:26 +0300

    lesson07JavaCore

---
+++ src/main/java/lesson7/WeatherModel.java
@@ -0,0 +1,12 @@
+package lesson7;
+
+import lesson7.entity.Weather;
+
+import java.io.IOException;
+import java.util.List;
+
+public interface WeatherModel {
+    void getWeather(String selectedCity, Period period) throws IOException;
+
+    public List<Weather> getSavedToDBWeather();
+}
